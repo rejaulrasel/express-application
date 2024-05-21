1f9931ea42acc45b79425a0e3ab63eead1a16a96
rejaulrasel <rejaulhasanrasel28@gmail.com>
2024-05-21 18:46:27 +0600

    setup datavase_url and env file

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,8 @@
-const express = require('express')
+import express, { Request, Response } from 'express'
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response) => {
     res.send('Hello World!')
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
