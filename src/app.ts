2dd804fb7afa5ddffb6ad2105c1d90f87fe7928c
rejaulrasel <rejaulhasanrasel28@gmail.com>
2024-05-22 08:51:40 +0600

    complete dotenv file configure and connect mongosse successfully

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,6 @@ const app = express()
 const port = 3000
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World!')
+    res.send('ASSALAMUALAIKUM!')
 })
 
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,24 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import config from './app/config';
 const app = express()
 const port = 3000;
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+//database connection
+async function main() {
+    try {
+        await mongoose.connect(config.database_url as string);
+
+        app.listen(config.port, () => {
+            console.log(`Example app listening on port ${config.port}`)
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+
+
+
+main()
