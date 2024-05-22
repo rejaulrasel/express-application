d08a8b02d814a2045319147435e621862dd1371f
rejaulrasel <rejaulhasanrasel28@gmail.com>
2024-05-22 20:39:54 +0600

    edit  order by email setup

--- src/app/modules/order/order..model.ts
+++ src/app/modules/order/order..model.ts
@@ -4,8 +4,8 @@ import { Order } from "./order.interface";
 
 
 const OrderSchema = new Schema({
-    email: { type: String, required: true, unique: true },
-    productId: { type: String, required: true, unique: true },
+    email: { type: String, required: true },
+    productId: { type: String, required: true },
     price: { type: Number, required: true },
     quantity: { type: Number, required: true }
 });
